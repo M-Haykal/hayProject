02cf4c9391897c15bef7cf3e1d13c2e2110294a3
M-Haykal <haykalmuhammad456@gmail.com>
2026-01-11 00:46:01 +0700

    Style: Changes themes portfolio

---
+++ src/data/files.js
@@ -0,0 +1,26 @@
+export const files = [
+  {
+    id: "about",
+    name: "About.js",
+    icon: "javascript",
+    color: "text-yellow-500",
+    component: "about",
+    path: ["src", "pages"],
+  },
+  {
+    id: "projects",
+    name: "Projects.py",
+    icon: "html",
+    color: "text-orange-400",
+    component: "projects",
+    path: ["src", "pages"],
+  },
+  {
+    id: "contact",
+    name: "Contact.ts",
+    icon: "php",
+    color: "text-purple-500",
+    component: "contact",
+    path: ["src", "pages"],
+  },
+];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,28 @@ module.exports = {
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./public/**/*.html",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#136dec",
+        "background-light": "#f6f7f8",
+        "background-dark": "#101822",
+        "editor-bg": "#1e1e1e",
+        "sidebar-bg": "#181818",
+        "activity-bg": "#333333",
+      },
+      fontFamily: {
+        display: ["Space Grotesk", "sans-serif"],
+        mono: ["Fira Code", "monospace"],
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px",
+      },
+    },
   },
   plugins: [],
 };
